e9eb966fee27daef29c62c0414260910a70a6631
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-05 23:48:21 +0530

    sucessfully installed firebase to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyATmP27k_0eMiHCrxCE8vojGi5BgD3j0hw",
+    authDomain: "organicshop-2a12c.firebaseapp.com",
+    databaseURL: "https://organicshop-2a12c.firebaseio.com",
+    projectId: "organicshop-2a12c",
+    storageBucket: "organicshop-2a12c.appspot.com",
+    messagingSenderId: "1078222206637"
+  },
 };
