e44a33fb977da411feff17aff960534059e3c096
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-06 00:26:37 +0530

    generated basic scaffolding and components for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,30 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BsNavbarComponent
+    BsNavbarComponent,
+    ProductsComponent,
+    LoginComponent,
+    HomeComponent,
+    CheckOutComponent,
+    MyOrdersComponent,
+    OrderSuccessComponent,
+    ShopingCartComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
   ],
   imports: [
     BrowserModule,
