f20f2e3724f41ae3b6ced878ea9efd1681c71235
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-06 13:50:04 +0530

    showing links as per user userstate

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-
-  constructor(private afAuth:AngularFireAuth) { }
+  user$:Observable<firebase.User>;
+  constructor(private afAuth:AngularFireAuth) { 
+      this.user$ = afAuth.authState;
+  }
   logout(){
     this.afAuth.auth.signOut();
   }
