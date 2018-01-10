47db194f612fa2fb6e95eb72c5556b6dbd283969
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-10 12:53:10 +0530

    update butttons rendering in product- card

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -21,6 +21,6 @@ export class ProductCardComponent {
     if(!this.shoppingCart) return 0;
 
     let item = this.shoppingCart.items[this.product.$key];
-    return item ? item.quantity : null;
+    return item ? item.quantity : 0;
   }
 }
