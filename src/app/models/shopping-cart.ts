0103b56b246ea9afee7d4b8653bae67e3eec3ace
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-10 19:23:41 +0530

    rendering total price in shopping-cart

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -3,14 +3,22 @@ import { ShoppingCartItem } from "./shopping-cart-item";
 export class ShoppingCart {
     items:ShoppingCartItem[]=[];
     constructor(public itemsMap:{[productId:string]:ShoppingCartItem}) {
-        for(let productId in itemsMap)
-            this.items.push(itemsMap[productId]);
+        for(let productId in itemsMap){
+            let item = itemsMap[productId]
+            this.items.push(new ShoppingCartItem(item.product,item.quantity));
+        }
     }
     
     get productIds() {
         return Object.keys(this.items);
     }
     
+    get totalPrice(){
+        let sum = 0;
+        this.items.forEach(item => sum+= item.totalPrice);
+        return sum;
+    }
+
     get itemCount(){
         let count = 0;
         for(let productId in this.items)
