fe9731bca77ebd95dd6345fdcce1b62a93ee2b97
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-27 12:38:58 +0530

    added search bar for admin products

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy} from '@angular/core';
 import { OrderService } from '../../services/order.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
-  orders$;
-  constructor(private orderService:OrderService) { }
+export class AdminOrdersComponent implements OnDestroy {
+  orders;
+  filteredOrders;
+  subscription:Subscription;
+  constructor(private orderService:OrderService) {
+    this.subscription =  this.orderService.getOrders().subscribe(orders=>{
+      this.filteredOrders = this.orders = orders;
+    });
+   }
 
-  ngOnInit() {
-    this.orders$ = this.orderService.getOrders();
-  }
+   ngOnDestroy(){
+     this.subscription.unsubscribe();
+   }
 
+   filter(query:String){
+     this.filteredOrders = (query)?this.orders.filter(o=>o.shipping.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())):this.orders;
+   }
 }
