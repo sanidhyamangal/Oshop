a5c87020dd3cbd203ffce3fc9d00665f53471019
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-27 12:20:28 +0530

    added search bar to manage admins

--- src/app/admin/manage-admins/manage-admins.component.ts
+++ src/app/admin/manage-admins/manage-admins.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-manage-admins',
   templateUrl: './manage-admins.component.html',
   styleUrls: ['./manage-admins.component.css']
 })
-export class ManageAdminsComponent implements OnInit {
+export class ManageAdminsComponent implements OnDestroy {
 
-  users$;
-  constructor( private userService:UserService) { }
-
-  ngOnInit() {
-    this.users$ = this.userService.getAll();
+  filteredUser;
+  users;
+  subscription:Subscription;
+  constructor( private userService:UserService) {
+    this.subscription = this.userService.getAll().subscribe(users=>{
+      this.filteredUser = this.users = users;
+    });
+   }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
   toggleAdmin(user){
     this.userService.toggleAdmin(user.$key);
+  }
+  filter(query:string){
+    this.filteredUser = (query)?this.users.filter(u=>u.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())):this.users;
   }
 }
